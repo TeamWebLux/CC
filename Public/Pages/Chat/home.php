3e0ce1c5a231b611623e1a3d04a981ba7aff2ffc
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-16 19:37:37 +0530

    Update home.php

--- Public/Pages/Chat/home.php
+++ Public/Pages/Chat/home.php
@@ -95,8 +95,7 @@
 			background-color: aqua;
 			color: black;
 		}
-
-		.rtext {
+		.rtext{
 			background-color: blue;
 			color: aliceblue;
 		}
@@ -184,9 +183,12 @@
 						</div>
 					<?php } ?>
 				</div>
-				<div class="input-group">
-					<label for="msg">Enter Your Message</label>
-					<input type="text" class="form-control" name="" id="message" aria-describedby="helpId" placeholder="">
+				<div class="input-group mb-3">
+					<div class="form-group">
+					  <label for="msg">Enter Your Message</label>
+					  <input type="text"
+						class="form-control" name="" id="message" aria-describedby="helpId" placeholder="">
+					</div>
 					<!-- <textarea cols="3" id="message" class="form-control"></textarea> -->
 					<button class="btn btn-primary" id="sendBtn">
 						<i class="fa fa-paper-plane">Send</i>
