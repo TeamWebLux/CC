0fffc2881be9e0adeb4af23fbc3223c0c8ec499f
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-17 18:19:43 +0530

    sdasfgrhtjyuio

--- Public/Pages/Common/scripts.php
+++ Public/Pages/Common/scripts.php
@@ -83,3 +83,4 @@
    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-vxI2WT6n5JBOH1bwe3D2CyPkJ9XoWKc9pu3Uip/rB9zW1YJqzgRbvm6YlLOf6fDI" crossorigin="anonymous">
