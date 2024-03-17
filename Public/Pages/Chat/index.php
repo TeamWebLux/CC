99e88231a1e636349d18294303181cbe585a4fbc
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-17 21:19:18 +0530

    Update index.php

--- Public/Pages/Chat/index.php
+++ Public/Pages/Chat/index.php
@@ -38,7 +38,7 @@
         if ($_SESSION['role'] == 'User') {
             // Fetch online agents in the same page
             $pagename = $_SESSION['page'];
-            $sql = "SELECT * FROM user WHERE role = 'Agent' AND last_seen(last_seen_column) = 'Active' AND pagename = ?";
+            $sql = "SELECT * FROM user WHERE role = 'Agent' AND last_seen(last_seen) = 'Active' AND pagename = ?";
             $stmt = $conn->prepare($sql);
             $stmt->execute([$pagename]);
             $agents = $stmt->fetchAll(PDO::FETCH_ASSOC);
