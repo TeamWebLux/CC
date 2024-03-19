780b99d1262de43115d67130de94b426edde06c5
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-19 21:13:22 +0530

    Update timezone_detect.js

--- Public/Pages/Chat/timezone_detect.js
+++ Public/Pages/Chat/timezone_detect.js
@@ -1,6 +1,7 @@
 // JavaScript to fetch timezone and send it back to the server via AJAX
 document.addEventListener('DOMContentLoaded', function() {
     var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log(timezone);
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'set_timezone.php', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
