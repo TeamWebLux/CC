9b6ed43f840c6e2d18f5b89fbe7f14d89ae280e3
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-19 19:08:49 +0530

    Update home.php

--- Public/Pages/Chat/home.php
+++ Public/Pages/Chat/home.php
@@ -253,11 +253,11 @@
 		/* Small devices (phones, 600px and down) */
 		@media (max-width: 600px) {
 
-			.w-400{
+			.w-400,
+			.w-10{
 				width: 100%;
 			}
-			.w-15,
-			.w-10 {
+			.w-15 {
 				width: 50%;
 				/* Full width */
 			}
