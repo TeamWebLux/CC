b8cce57f4bd6b3b6c3ae0abb374c2ba95eb44e29
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-24 02:30:44 +0530

    Update pagelist.php

--- Public/Pages/Chat/pagelist.php
+++ Public/Pages/Chat/pagelist.php
@@ -116,7 +116,7 @@
                                 $branchId = $branchRow['bid']; // Now you have the branch ID
 
                                 // Use the branch ID to fetch pages
-                                if($_SESSION['role']==='Admin'){
+                                if($_SESSION['role']='Admin'){
                                     $pageQuery = "SELECT * FROM page "; // Adjust the table name if necessary
 
 
