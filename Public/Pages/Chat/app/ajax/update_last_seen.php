aa18052afda31dfdb24ec588beba0d27fa1ab723
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-24 03:36:55 +0530

    Update update_last_seen.php

--- Public/Pages/Chat/app/ajax/update_last_seen.php
+++ Public/Pages/Chat/app/ajax/update_last_seen.php
@@ -8,14 +8,17 @@ if (isset($_SESSION['username'])) {
 	# database connection file
 	include '../db.conn.php';
 
-	# get the logged in user's username from SESSION
+	# get the logged in user's id and time zone from SESSION
 	$id = $_SESSION['user_id'];
+	$timeZone = $_SESSION['timezone']; // Assuming you have time zone information in the session
 
+	# SQL to update last_seen and time_zone
 	$sql = "UPDATE user
-	        SET last_seen = NOW() 
+	        SET last_seen = NOW(), 
+	            time_zone = ?
 	        WHERE id = ?";
 	$stmt = $conn->prepare($sql);
-	$stmt->execute([$id]);
+	$stmt->execute([$timeZone, $id]);
 
 }else {
 	header("Location: ../../index.php");
