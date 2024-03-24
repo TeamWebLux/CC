7565ea2c0899a5204668e76a8e50b879feb97237
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-24 14:18:01 +0530

    Update scripts.php

--- Public/Pages/Portal/scripts.php
+++ Public/Pages/Portal/scripts.php
@@ -4,6 +4,15 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" rel="stylesheet">
 
+<!-- jQuery Library - Load this first to ensure it's available for all jQuery-dependent scripts -->
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+
+<!-- Toastr Notification Library - Depends on jQuery, so it comes after jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<!-- Bootstrap Bundle JS - Includes Popper; necessary for Bootstrap components; comes after jQuery -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 <!-- Core Library and Plugin Scripts -->
 <script src="../assets/js/core/libs.min.js"></script>
 <script src="../assets/js/core/external.min.js"></script>
@@ -22,12 +31,3 @@
 <script src="../assets/js/pages/chat-popup.js"></script>
 <script src="../assets/js/template.js"></script>
 <script src="../assets/js/vendor_components/datatable/datatables.min.js"></script>
-
-<!-- Toastr Notification Library -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-<!-- jQuery Library -->
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-
-<!-- Bootstrap Bundle JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
