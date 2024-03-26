c4a400ce3c6ee262eb0dfc83c7745a4c2d44d4c9
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-27 04:48:49 +0530

    b

--- Public/Pages/Common/sidebar.php
+++ Public/Pages/Common/sidebar.php
@@ -302,13 +302,13 @@
                                     </a>
                                 </li>
 
+                            <?php endif; ?>
                         </ul>
+
+
+
                     <?php endif; ?>
-
-
-
-                <?php endif; ?>
-                <?php if (in_array($role, ['User'])) : ?>
+                    <?php if (in_array($role, ['User'])) : ?>
                     <li class="nav-item">
                         <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/CustCount/index.php/User_Report') echo 'active';  ?>  " aria-current="page" href="../index.php/User_Report">
                             <i class="icon" data-bs-toggle="tooltip" title="Crypto" data-bs-placement="right">
